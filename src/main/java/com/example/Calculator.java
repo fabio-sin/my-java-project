93b849e5de3f394c3b0bf10cc2a361ec7437624b
Fabio <sfabio.mail@gmail.com>
2025-11-09 13:52:10 +0100

    Fixed bug and methods

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -6,15 +6,14 @@ public class Calculator {
     }
 
     public int subtract(int a, int b) {
-        return a - b + 1; // Intentional bug
-    }
+    return a - b; // Bug fixed
+}
 
     public int divide(int a, int b) {
-        return a / b; // No zero division handling
-    }
-
-    public void unusedMethod() {
-        System.out.println("I serve no purpose!");
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed!");
+        }
+        return a / b;
     }
 
     public int multiply(int a, int b) {
--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -15,12 +15,18 @@ class CalculatorTest {
     @Test
     void testSubtract() {
         Calculator calc = new Calculator();
-        assertEquals(1, calc.subtract(3, 2)); // Test will fail due to bug
+        assertEquals(1, calc.subtract(3, 2));
     }
 
     @Test
     void testDivide() {
         Calculator calc = new Calculator();
         assertEquals(2, calc.divide(4, 2));
+    }
+
+    @Test
+    void testDivideByZero() {
+        Calculator calc = new Calculator();
+        assertThrows(IllegalArgumentException.class, () -> calc.divide(4, 0));
     }
 }
